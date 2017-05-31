b03a296348c8365486ecc51ac6f10701017eafe2
Tomek Bujakowski <bujakowski@gmail.com>
2017-06-01 01:14:51 +0200

    Refactor testFindAllCategories with mocks

--- tests/com/codecool/shop/dao/ProductCategoryDaoSqliteTest.java
+++ tests/com/codecool/shop/dao/ProductCategoryDaoSqliteTest.java
@@ -5,12 +5,12 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.Spy;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -52,14 +52,18 @@ class ProductCategoryDaoSqliteTest {
 
     @Test
     void testFindAllCategories() {
-        List<ProductCategory> categories = productCategoryDao.getAll();
+        ProductCategory productCategory = mock(ProductCategory.class);
+        List<ProductCategory> spiedCategories = Mockito.spy(new ArrayList<>());
+
+        spiedCategories.add(productCategory);
+        Mockito.verify(spiedCategories).add(productCategory);
+
+        Mockito.doReturn(productCategory).when(spiedCategories).get(2);
+        when(spiedCategories.get(2).getDepartment()).thenReturn("neutralna Szwajcaria");
+
         assertEquals(
-                Arrays.asList(
-                    new ProductCategory(1,"chemia", "niemieckie środki czystości i nie tylko", "Berlin"),
-                    new ProductCategory(2,"spożywcze", "tylko świeże warzywa i owoce", "Hala Targowa"),
-                    new ProductCategory(3,"narzędzia", "narzędzia sprawne jak szwajcarskie zegarki", "neutralna Szwajcaria"),
-                    new ProductCategory(4,"słodycze", "wszystko to co misie lubią najbardziej", "Dom Kubusia Puchatka")
-                ).get(2).getDepartment(),
-                categories.get(2).getDepartment());
+                spiedCategories.get(2).getDepartment(),
+                productCategoryDao.getAll().get(2).getDepartment()
+        );
     }
 }
