fa81a515edbdacf8f9959e76af3ca5c9ef38c29f
Tomek Bujakowski <bujakowski@gmail.com>
2017-06-01 00:18:58 +0200

    Add testFindAllSuppliers in SupplierDaoSqliteTest

--- tests/com/codecool/shop/dao/SupplierDaoSqliteTest.java
+++ tests/com/codecool/shop/dao/SupplierDaoSqliteTest.java
@@ -4,17 +4,28 @@ import com.codecool.shop.model.Supplier;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SupplierDaoSqliteTest {
+    @Mock
     private SQLiteJDBCConnector connector;
+    @Mock
     private SupplierDao supplierDao;
+    @Spy
+    List<Supplier> spiedSuppliers =  new ArrayList<>();
 
     @BeforeEach
     void setUp() throws SQLException {
+        MockitoAnnotations.initMocks(this);
         connector = new SQLiteJDBCConnector();
         connector.setDatabaseFilePath("jdbc:sqlite:src/main/resources/test_database.db");
         connector.connectToDb();
@@ -33,5 +44,11 @@ class SupplierDaoSqliteTest {
     void testFindSupplierById() {
         Supplier supplier = new Supplier(1, "Ariel", "Najczerwieńsza jakość");
         assertEquals(supplier.getDescription(), supplierDao.find(1).getDescription());
+    }
+
+    @Test
+    void testFindAllSuppliers() {
+        Mockito.doReturn(8).when(spiedSuppliers).size();
+        assertEquals(spiedSuppliers.size(), supplierDao.getAll().size());
     }
 }
