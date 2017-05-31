cb04825c8bf3548312382c247c1ffde797888271
Tomek Bujakowski <bujakowski@gmail.com>
2017-05-31 22:08:43 +0200

    Add BeforeEach and AfterEach in SupplierDaoSqliteTest

--- tests/com/codecool/shop/dao/SupplierDaoSqliteTest.java
+++ tests/com/codecool/shop/dao/SupplierDaoSqliteTest.java
@@ -1,8 +1,29 @@
 package com.codecool.shop.dao;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.sql.SQLException;
+
+
 class SupplierDaoSqliteTest {
     private SQLiteJDBCConnector connector;
     private SupplierDao supplierDao;
 
+    @BeforeEach
+    void setUp() throws SQLException {
+        connector = new SQLiteJDBCConnector();
+        connector.setDatabaseFilePath("jdbc:sqlite:src/main/resources/test_database.db");
+        connector.connectToDb();
+        connector.dropTables();
+        connector.createTables();
+        connector.fillTables();
+        supplierDao = new SupplierDaoSqlite(connector.getConnection());
+    }
+
+    @AfterEach
+    void close() throws SQLException {
+        connector.getConnection().close();
+    }
 
 }
