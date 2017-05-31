f0d9f114fa9ab61e1ce33257e9cdaabfb28d5251
Tomek Bujakowski <bujakowski@gmail.com>
2017-05-31 16:24:09 +0200

    Add testFindCategoryById in ProductCategoryDaoSqliteTest

--- tests/com/codecool/shop/dao/ProductCategoryDaoSqliteTest.java
+++ tests/com/codecool/shop/dao/ProductCategoryDaoSqliteTest.java
@@ -1,9 +1,13 @@
 package com.codecool.shop.dao;
 
+import com.codecool.shop.model.ProductCategory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ProductCategoryDaoSqliteTest {
 
@@ -24,5 +28,11 @@ class ProductCategoryDaoSqliteTest {
     @AfterEach
     void close() throws SQLException {
         connector.getConnection().close();
+    }
+
+    @Test
+    void testFindCategoryById() {
+        ProductCategory category = new ProductCategory(1, "chemia", "niemieckie środki czystości i nie tylko", "Berlin");
+        assertEquals(category, productCategoryDao.find(-1));
     }
 }
