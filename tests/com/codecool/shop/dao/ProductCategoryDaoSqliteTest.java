6ba2a62d3cfc413a478f2b17b18bd7377e78f643
Tomek Bujakowski <bujakowski@gmail.com>
2017-05-31 16:23:04 +0200

    Add BeforeEach and AfterEach in ProductCategoryDaoSqliteTest

--- tests/com/codecool/shop/dao/ProductCategoryDaoSqliteTest.java
+++ tests/com/codecool/shop/dao/ProductCategoryDaoSqliteTest.java
@@ -1,5 +1,28 @@
 package com.codecool.shop.dao;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.sql.SQLException;
+
 class ProductCategoryDaoSqliteTest {
 
+    private ProductCategoryDao productCategoryDao;
+    private SQLiteJDBCConnector connector;
+
+    @BeforeEach
+    void setUp() throws SQLException {
+        connector = new SQLiteJDBCConnector();
+        connector.setDatabaseFilePath("jdbc:sqlite:src/main/resources/test_database.db");
+        connector.connectToDb();
+        connector.dropTables();
+        connector.createTables();
+        connector.fillTables();
+        productCategoryDao = new ProductCategoryDaoSqlite(connector.getConnection());
+    }
+
+    @AfterEach
+    void close() throws SQLException {
+        connector.getConnection().close();
+    }
 }
