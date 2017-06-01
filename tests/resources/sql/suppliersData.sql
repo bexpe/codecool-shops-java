a3c0a5717fae6e7634c9fc280fa0f1c8ee594c29
Tomek Bujakowski <bujakowski@gmail.com>
2017-06-01 12:35:42 +0200

    Add SQL files with test data in tests resources

---
+++ tests/resources/sql/suppliersData.sql
@@ -0,0 +1,2 @@
+INSERT INTO suppliers (name, description) VALUES ('SUPPLIER 1', 'DESCRIPTION SUPPLIER 1');
+INSERT INTO suppliers (name, description) VALUES ('SUPPLIER 2', 'DESCRIPTION SUPPLIER 2');
