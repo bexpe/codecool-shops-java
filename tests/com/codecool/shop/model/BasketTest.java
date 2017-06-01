433136634bfb5fed4e26539e2d58e3f9850a359d
sebmichalowski <seb.michalowski@gmail.com>
2017-06-01 15:30:57 +0200

    BasketTest setUp() implemented

--- tests/com/codecool/shop/model/BasketTest.java
+++ tests/com/codecool/shop/model/BasketTest.java
@@ -1,10 +1,24 @@
 package com.codecool.shop.model;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
 
 /**
  * Created by beata on 29.05.17.
  */
 class BasketTest {
+    private Basket basket;
+    private BasketItem basketItem;
+    private Product product;
+    private List<BasketItem> basketItemList;
 
+    @BeforeEach
+    void setUp() {
+        basket = new Basket();
+        basketItem = mock(BasketItem.class);
+        product = mock(Product.class);
+    }
 }
