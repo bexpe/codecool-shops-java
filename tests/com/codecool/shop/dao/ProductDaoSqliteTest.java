dd0a4a235f785f02f1bb79ac468fad3d21271daa
Tomek Bujakowski <bujakowski@gmail.com>
2017-06-01 15:40:00 +0200

    Add testFindProductById in ProductDaoSqliteTest

--- tests/com/codecool/shop/dao/ProductDaoSqliteTest.java
+++ tests/com/codecool/shop/dao/ProductDaoSqliteTest.java
@@ -1,10 +1,16 @@
 package com.codecool.shop.dao;
 
+import com.codecool.shop.model.Product;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 
 import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class ProductDaoSqliteTest extends BaseTest {
 
@@ -28,4 +34,13 @@ class ProductDaoSqliteTest extends BaseTest {
         super.closeDB();
     }
 
+    @Test
+    void testFindProductById() {
+        Product product = mock(Product.class);
+        when(product.getDescription()).thenReturn("DESCRIPTION 2");
+        assertEquals(
+                product.getDescription(),
+                productDao.find(2).getDescription()
+        );
+    }
 }
