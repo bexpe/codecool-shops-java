68939dca577d0b30a9b0865628b4142825ce4499
Tomek Bujakowski <bujakowski@gmail.com>
2017-06-01 14:55:58 +0200

    Change productDao constructor call in setUpDB() in ProductDaoSqlite test

--- tests/com/codecool/shop/dao/ProductDaoSqliteTest.java
+++ tests/com/codecool/shop/dao/ProductDaoSqliteTest.java
@@ -2,13 +2,25 @@ package com.codecool.shop.dao;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mock;
 
 import java.sql.SQLException;
 
 class ProductDaoSqliteTest extends BaseTest {
+
+    @Mock
+    private ProductDao productDao;
+    @Mock
+    private SupplierDao supplierDao;
+    @Mock
+    private ProductCategoryDao productCategoryDao;
+
     @BeforeEach
     void setUp() throws SQLException {
         super.setUpDB();
+        productDao = new ProductDaoSqlite(connector.getConnection(),
+                new SupplierDaoSqlite(connector.getConnection()),
+                new ProductCategoryDaoSqlite(connector.getConnection()));
     }
 
     @AfterEach
