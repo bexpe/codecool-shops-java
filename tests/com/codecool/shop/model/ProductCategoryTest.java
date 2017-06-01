eddfad1f45eec3872a7b698f591bdb2dacc1882e
sebmichalowski <seb.michalowski@gmail.com>
2017-06-01 02:44:32 +0200

    ProductCategoryTest testToString() implemented

--- tests/com/codecool/shop/model/ProductCategoryTest.java
+++ tests/com/codecool/shop/model/ProductCategoryTest.java
@@ -1,10 +1,28 @@
 package com.codecool.shop.model;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Created by beata on 29.05.17.
  */
 class ProductCategoryTest {
+
+    @Test
+    void testToString() {
+        ProductCategory productCategory = new ProductCategory(
+                1,
+                "name",
+                "description",
+                "department");
+
+        assertEquals(
+                "id: 1," +
+                        "name: name, " +
+                        "department: department, " +
+                        "description: description",
+                productCategory.toString());
+    }
 
 }
