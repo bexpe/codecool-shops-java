a9c6533c4e8dbb7a80c74e85f5769f072b581040
Tomek Bujakowski <bujakowski@gmail.com>
2017-06-01 21:57:15 +0200

    Add testConnectingToDBWhenPathIsIncorrect

--- tests/com/codecool/shop/dao/SQLiteJDBCConnectorTest.java
+++ tests/com/codecool/shop/dao/SQLiteJDBCConnectorTest.java
@@ -2,6 +2,16 @@ package com.codecool.shop.dao;
 
 import org.junit.jupiter.api.Test;
 
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 class SQLiteJDBCConnectorTest {
 
+    @Test
+    void testConnectingToDBWhenPathIsIncorrect() throws SQLException {
+        SQLiteJDBCConnector connector = new SQLiteJDBCConnector();
+        connector.setDatabaseFilePath("jdbc:sqlite:tests/wrong_directory/resources/test_database.db");
+        assertThrows(RuntimeException.class, connector::connectToDb);
+    }
 }
